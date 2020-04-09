a25c6adbf0036c446ce84e1d80e1ff7277b70755
datsciseol <dat.sci.seol@gmail.com>
2020-04-09 17:29:56 +0900

    done

--- 0409/10974.py
+++ 0409/10974.py
@@ -1,4 +1,21 @@
-from itertools import permutations
-num = int(input())
-for perm in permutations(range(1, num + 1)):
-    print(*perm)
+def next_permutations(num_list):
+    iter = len(num_list) - 1
+    while iter > 0 and num_list[iter - 1] >= num_list[iter]:
+        iter -= 1
+    if iter <= 0:
+        return False
+    jter = len(num_list) - 1
+    while num_list[jter] <= num_list[iter - 1]:
+        jter -= 1
+    num_list[iter - 1], num_list[jter] = num_list[jter], num_list[iter - 1]
+    kter = len(num_list) - 1
+    while iter < kter:
+        num_list[iter], num_list[kter] = num_list[kter], num_list[iter]
+        iter += 1
+        kter -= 1
+    return True
+N = int(input())
+num_list = list(range(1, N + 1))
+print(*num_list)
+while next_permutations(num_list):
+    print(*num_list)
